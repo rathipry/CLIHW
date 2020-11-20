177008ff7c53fbd72ef6663a06654e6e82416c91
RathipriyaSingaram <72462005+rathipry@users.noreply.github.com>
2020-11-20 08:53:18 +0530

    [Rathipriya]first repo to push CLI assignments

---
+++ add.sh
@@ -0,0 +1,8 @@
+echo 'Enter first number to add : '
+read num1
+echo 'enter second number to add : '
+read num2
+sum=$((num1 + num2))
+#to find the sum directly in command line instead of script
+# expr 2 + 3
+echo 'the sum is ' $sum
